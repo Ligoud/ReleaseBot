d3e6e0fe34bc7f8a62cc6adf79ecd2dbbb55b7f4
Ligoud <43087993+Ligoud@users.noreply.github.com>
2019-11-15 19:21:38 +0400

    sss

--- bot.js
+++ bot.js
@@ -39,9 +39,10 @@ class MyBot extends ActivityHandler {
             var words=text.split(' ');
             //убираю меншн. (тут название зарегистрированного бота)
             //await context.sendActivity('Первое слово: '+words[0])  
-            await context.sendActivity(JSON.stringify(context.activity.conversation.isGroup));
-            var check=context.activity.conversation.isGroup
-            await context.sednActivity('true')
+            await context.sendActivity(JSON.stringify(context.activity.conversation));
+            await context.sednActivity(words[0])
+            if(words[0]=='ofmbot')
+                await context.sednActivity('true')
             /*if(){
                await context.sendActivity('wtf')    
                words.shift()
