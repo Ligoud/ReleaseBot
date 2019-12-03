704f5c10ba90e2d3e728cce411bf7fa78d6d25cd
Ligoud <43087993+Ligoud@users.noreply.github.com>
2019-12-03 22:31:14 +0400

    upd2

--- DataBase/mongo.js
+++ DataBase/mongo.js
@@ -2,45 +2,45 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://user:userIndeed@offsmngrbot-yfbnk.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect()
-
+const dbname='test'
 class Mongo{
     constructor(){}
 
     async createIfNotExists(colName)      //Создает коллекцию, если ее не было
     {
         try{
-            await client.db('test').createCollection(colName)
+            await client.db(dbname).createCollection(colName)
         }
         catch(err){}
     }
     async add(colName,item) //Кладет 1 итем
     {        
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(dbname).collection(colName)
         await col.insertOne(item)    
     }
     async read(colName,query)   //Массив возвращает
     {
         await this.createIfNotExists(colName)
-        const col= client.db('bot').collection(colName)
+        const col= client.db(dbname).collection(colName)
         const res=await col.find(query).toArray();   
         return res
     }
     async delete(colName,filter)
     {
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(dbname).collection(colName)
         await col.deleteOne(filter)
     }
     async update(colName,idObj,updFilter){
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(dbname).collection(colName)
         await col.updateOne(idObj,updFilter)
     }
     async replace(colName,filter,repObj)
     {
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(dbname).collection(colName)
         await col.replaceOne(filter,repObj)
     }
 }
