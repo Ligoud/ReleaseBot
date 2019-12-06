4201128994e2c892cef7ee09ad86c3caee7d49a8
Ligoud <43087993+Ligoud@users.noreply.github.com>
2019-12-06 22:04:15 +0400

    this.dbname constructor

--- DataBase/mongo.js
+++ DataBase/mongo.js
@@ -4,43 +4,45 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect()
 
 class Mongo{
-    constructor(){}
+    constructor(db){
+        this.dbname=db
+    }
 
     async createIfNotExists(colName)      //Создает коллекцию, если ее не было
     {
         try{
-            await client.db('bot').createCollection(colName)
+            await client.db(this.dbname).createCollection(colName)
         }
         catch(err){}
     }
     async add(colName,item) //Кладет 1 итем
     {        
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(this.dbname).collection(colName)
         await col.insertOne(item)    
     }
     async read(colName,query)   //Массив возвращает
     {
         await this.createIfNotExists(colName)
-        const col= client.db('bot').collection(colName)
+        const col= client.db(this.dbname).collection(colName)
         const res=await col.find(query).toArray();   
         return res
     }
     async delete(colName,filter)
     {
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(this.dbname).collection(colName)
         await col.deleteOne(filter)
     }
     async update(colName,idObj,updFilter){
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(this.dbname).collection(colName)
         await col.updateOne(idObj,updFilter)
     }
     async replace(colName,filter,repObj)
     {
         await this.createIfNotExists(colName)
-        const col=client.db('bot').collection(colName)
+        const col=client.db(this.dbname).collection(colName)
         await col.replaceOne(filter,repObj)
     }
 }
